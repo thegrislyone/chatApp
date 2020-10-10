6cc45a3c516e8c32fdef2522983d093a03763c54
Roman <roma.leviczkij@bk.ru>
2020-10-11 00:10:39 +0300

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>
+      HEADLINE
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  sockets: {
+    connect: function() {
+      console.log("Socket connected");
+    }
+  },
+}
+</script>
